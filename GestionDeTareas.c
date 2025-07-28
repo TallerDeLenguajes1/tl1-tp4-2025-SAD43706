bb8ec5607beab2a7dd7361dd4278ccbd7e230d0d
Santiago Dominguez <santiagodominguezarj@gmail.com>
2025-07-28 14:24:18 -0300

    Cracion de las Funciones para Crear, Insertar y Mostrar la Lista

--- GestionDeTareas.c
+++ GestionDeTareas.c
@@ -1,35 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 struct Tarea{
     int TareaID;//Numérico autoincremental comenzando en 1000
     char *Descripcion; //
     int Duracion; // entre 10 – 100
 }typedef Tarea;
-struct Nodo{
+typedef struct Nodo{
     Tarea T;
-    Nodo *Siguiente;
-}typedef Nodo;
-void mostrarLists();
+    struct Nodo *Siguiente;
+}Nodo;
 Nodo *crearLista();
-Nodo *CrearNodo(Tarea T);
 Tarea cargarTarea();
+Nodo *CrearNodo(Tarea T);
+void enLista(Nodo **Inicio,Nodo *Nodo);
+void mostrarLists();
 int main(){
     char estado='s';
+    int idx=1000;
     Nodo *InicioP;
     Nodo *InicioR;
     InicioP=crearLista();
     InicioR=crearLista();
     do
     {
-       Tarea TareaX=cargarTarea();
-       Nodo *nodo=CrearNodo(TareaX);
-
-    } while (estado=='s');
+       puts("Ingrese la Tarea Pendiente:");
+        Tarea TareaX=cargarTarea();
+        TareaX.TareaID=idx++;
+       Nodo *nuevo=CrearNodo(TareaX);
+       enLista(&InicioP,nuevo);
+       puts("Desea Ingresar otra tarea Pendiente(s/n)");
+       scanf(" %c",&estado);
+       getchar();
+    } while (tolower(estado)=='s');
     
 }
 Nodo *crearLista(){
     return NULL;
+}
+Tarea cargarTarea(){
+    Tarea T;
+
+    char des[500];
+    int duracion;
+    puts("Ingrese la Descrpcion de la Tarea a Realizar:");
+    fgets(des,500,stdin);
+    des[strcspn(des, "\n")] = '\0';//Mas seguro y no se corta despues del espacio fgets(desino,tamaño,stdin tecladox)
+    int tamDes=strlen(des)+1;
+    char *description;
+    description=(char*)malloc(tamDes*sizeof(char));
+    strcpy(description,des);
+    
+    puts("Ingrese la Duracion(10-100):");
+    scanf(" %i",&duracion);
+    getchar();
+
+    T.Descripcion=description;
+    T.Duracion=duracion;
+    
+    return T;
 }
 Nodo *CrearNodo(Tarea T)
 {
@@ -38,26 +68,30 @@ Nodo *CrearNodo(Tarea T)
     Nnodo->Siguiente = NULL;
     return Nnodo;
 }
-Tarea cargarTarea(){
-    Tarea T;
-
-    int id=1000;
-    
-    char des[500];
-    int duracion;
-    puts("Ingrese la Descrpcion de la Tarea a Realizar:");
-    gets(des);
-    int tamDes=strlen(des)+1;
-    char *description;
-    description=(char*)malloc(tamDes*sizeof(char));
-    strcpy(description,des);
-    
-    puts("Ingrese la Duracion(10-100):");
-    scanf("%i",&duracion);
-
-    T.TareaID=id++;
-    T.Descripcion=description;
-    T.Duracion=duracion;
-    
-    return T;
+void enLista(Nodo **Inicio,Nodo *nuevo){
+    nuevo->Siguiente=*Inicio;
+    *Inicio=nuevo;
+}
+void mostrarLists(Nodo *Inicio){
+    Nodo *nodoMos=Inicio;
+    int TamDes,i;
+    if (nodoMos==NULL)
+    {
+        printf("La Lista esta Vacia");
+    }
+    else
+    {
+        while (Inicio!=NULL)
+        {
+            printf("ID\n:%i",nodoMos->T.TareaID);
+            /*TamDes=strlen(nodoMos->T.Descripcion);
+            for ( i = 0; i < TamDes; i++)
+            {
+            
+            }*/
+            printf("DESCRIPCION:%s\n",nodoMos->T.Descripcion);
+            printf("DURACION:%i\n",nodoMos->T.Duracion);
+            nodoMos=nodoMos->Siguiente;
+        }    
+    }
 }
